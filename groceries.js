e11c65b5de297670d133915d38c420c25233401a
mbogeassan <48943261+mbogeassan@users.noreply.github.com>
2019-08-02 00:14:33 +0300

    I created an array of groceries and use the map function

---
+++ groceries.js
@@ -0,0 +1,8 @@
+var groceries = ['mango','banana','orange', 'pineapple','pear'];
+function capitalLetter(cap){
+    var firstLetter = cap.charAt(0).toUpperCase();
+    return  firstLetter + cap.slice(1);
+}
+
+var myNewGroceries = groceries.map(capitalLetter);
+console.log(myNewGroceries);
