a69663a0461d3be589e384ea1ba2c23aef975ea1
assanmboge <mbogeassan77@gmail.com>
2019-08-02 00:50:00 +0300

    adding Assan Variable

--- groceries.js
+++ groceries.js
@@ -1,5 +1,6 @@
 var groceries = ['mango','banana','orange', 'pineapple','pear'];
 var numbers [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+var Assan = 15;
 function capitalLetter(cap){
     var firstLetter = cap.charAt(0).toUpperCase();
     return  firstLetter + cap.slice(1);
